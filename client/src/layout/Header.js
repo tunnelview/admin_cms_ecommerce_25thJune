2b4262a27995411392b9919104b6886b7894777b
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-06-25 09:28:07 +1000

    Add navbar from react-bootstrap

--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -1,5 +1,21 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="light" expand="md">
+      <Container>
+        <Navbar.Brand href="#home">Admin CMS </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Login </Nav.Link>
+            <Nav.Link href="#link">Register </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
